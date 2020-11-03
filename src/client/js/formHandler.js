67caa7d689b991b5e182ff7cd6b0807908ea0631
MatthiasG42 <69195293+MatthiasG42@users.noreply.github.com>
2020-11-03 11:52:51 +0100

    style now in

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,6 +8,7 @@ function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/test')
+    //fetch('api.meaningcloud.com/sentiment-2.1?key=e9caf65530c914ff1792df2f6e1c0908&of=json&txt=What%20a%20great%20day%20it%20is%20!&model=general&lang=en')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
