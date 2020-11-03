c3ad01f9039cdcda5a4a498c998f9189928b17b8
MatthiasG42 <69195293+MatthiasG42@users.noreply.github.com>
2020-11-03 11:01:42 +0100

    Basic functions online
    sass and response

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,33 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
+
 const app = express()
+
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
 
 console.log(__dirname)
+
+console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -27,5 +45,6 @@ app.listen(8080, function () {
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(mockAPIResponse);
+    res.json(mockAPIResponse);
 })
