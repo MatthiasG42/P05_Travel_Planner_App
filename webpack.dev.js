ea11f66ac26ae6b7469ada39c5bc27788bc7c4cc
MatthiasG42 <69195293+MatthiasG42@users.noreply.github.com>
2020-11-09 09:35:22 +0100

    Review Update
    Updated based on comments on the version that passed

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin');
+//Workbox plugin not needed in dev phase
+//const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -48,6 +49,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new WorkboxPlugin.GenerateSW()
+        //new WorkboxPlugin.GenerateSW()
     ]
 }
