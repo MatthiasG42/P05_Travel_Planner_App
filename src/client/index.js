3b47207e067f4d54f0871b125fb3d0b896e2f5bb
MatthiasG42 <69195293+MatthiasG42@users.noreply.github.com>
2020-11-10 12:49:15 +0100

    first card design in the works

--- src/client/index.js
+++ src/client/index.js
@@ -10,6 +10,7 @@ import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+import './styles/trip_card.scss'
 
 
 
--- src/client/js/tripDataUpdate.js
+++ src/client/js/tripDataUpdate.js
@@ -1,6 +1,6 @@
 function tripUpdate(dates, geoData, weather, pictures, rest ){
     return `
-            <section class "trip_card" id="trip">
+            <section class= "trip_card" id="trip">
             <strong id="trip_headline">Your Trip</strong>
                 <div id="destination">You are going to the beautiful city of: ${geoData.city}</div>
                 <div id="first_picture"><img src="${pictures.hits[0].webformatURL}" alt""></div>
@@ -9,6 +9,17 @@ function tripUpdate(dates, geoData, weather, pictures, rest ){
                 <div id="weather">Your weather will be ${weather.data[0].weather.description}</div>
                 <div id="rest">${rest.name} has ${rest.capital} as its capital and a population of ${rest.population}</div>
                 <div id="irony"></div>
+
+                <div class="trip_card_days">
+                    <div class="Start">Trip Start</div>
+                    <div class="End">Trip End</div>
+                    <div class="Dur">Trip Duration</div>
+                    <div class="Days">Days Left</div>
+                    <div class="var_start">${dates.trip_start_date}</div>
+                    <div class="var_end">${dates.trip_end_date}</div>
+                    <div class="var_dur">${dates.trip_duration}</div>
+                    <div class="var_days">${dates.days_left}</div>
+                </div>
             </section>
             `
 }
