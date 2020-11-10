1b3d6a4af839a9af0d209011abd84bf5d64e14bc
MatthiasG42 <69195293+MatthiasG42@users.noreply.github.com>
2020-11-10 15:50:30 +0100

    First base
    Everything working and styled

--- src/client/js/tripDataUpdate.js
+++ src/client/js/tripDataUpdate.js
@@ -1,14 +1,10 @@
 function tripUpdate(dates, geoData, weather, pictures, rest ){
     return `
             <section class= "trip_card" id="trip">
-            <strong id="trip_headline">Your Trip</strong>
-                <div id="destination">You are going to the beautiful city of: ${geoData.city}</div>
-                <div id="first_picture"><img src="${pictures.hits[0].webformatURL}" alt""></div>
-                <div id="dates">You go from the ${dates.trip_start_date} to the ${dates.trip_end_date}</div>
-                <div id="timeframe">That is ${dates.days_left} days from today, and your trip duration is ${dates.trip_duration} days</div>
-                <div id="weather">Your weather will be ${weather.data[0].weather.description}</div>
-                <div id="rest">${rest.name} has ${rest.capital} as its capital and a population of ${rest.population}</div>
-                <div id="irony"></div>
+            
+                <div class="city_image" id="first_picture"><img src="${pictures.hits[0].webformatURL}" alt""></div>
+
+                <div class="trip_header">Your trip to ${geoData.city}</div>
 
                 <div class="trip_card_days">
                     <div class="Start">Trip Start</div>
@@ -20,9 +16,42 @@ function tripUpdate(dates, geoData, weather, pictures, rest ){
                     <div class="var_dur">${dates.trip_duration}</div>
                     <div class="var_days">${dates.days_left}</div>
                 </div>
+
+                <div class="trip_card_weather">
+                    <div class="w_date">Date</div>
+                    <div class="d1">${weather.data[0].datetime}</div>
+                    <div class="d2">${weather.data[1].datetime}</div>
+                    <div class="d3">${weather.data[2].datetime}</div>
+                    <div class="w_des">Weather Description</div>
+                    <div class="w_d1">${weather.data[0].weather.description}</div>
+                    <div class="w_d2">${weather.data[1].weather.description}</div>
+                    <div class="w_d3">${weather.data[2].weather.description}</div>
+                    <div class="w_hl">Temperature High / Low</div>
+                    <div class="whl_d1">${weather.data[0].max_temp}°C / ${weather.data[0].min_temp}°C</div>
+                    <div class="whl_d2">${weather.data[1].max_temp}°C / ${weather.data[1].min_temp}°C</div>
+                    <div class="whl_d3">${weather.data[2].max_temp}°C / ${weather.data[2].min_temp}°C</div>
+                </div>
+                <div class="trip_card_weather_page2">
+                    <div class="d4">${weather.data[3].datetime}</div>
+                    <div class="d5">${weather.data[4].datetime}</div>
+                    <div class="d6">${weather.data[5].datetime}</div>
+                    <div class="d7">${weather.data[6].datetime}</div>
+                    <div class="w_d4">${weather.data[3].weather.description}</div>
+                    <div class="w_d5">${weather.data[4].weather.description}</div>
+                    <div class="w_d6">${weather.data[5].weather.description}</div>
+                    <div class="w_d7">${weather.data[6].weather.description}</div>
+                    <div class="whl_d4">${weather.data[3].max_temp}°C / ${weather.data[3].min_temp}°C</div>
+                    <div class="whl_d5">${weather.data[4].max_temp}°C / ${weather.data[4].min_temp}°C</div>
+                    <div class="whl_d6">${weather.data[5].max_temp}°C / ${weather.data[5].min_temp}°C</div>
+                    <div class="whl_d7">${weather.data[6].max_temp}°C / ${weather.data[6].min_temp}°C</div>
+                </div>
+
+                <div class="rest_info" id="rest">
+                    <div>${rest.name} is a part of ${rest.subregion}</div>
+                    <div>${rest.capital} is the capital and the total population is ${rest.population/1000000} Million</div>
+                    <div>The main language is ${rest.languages[0].name} and at home the country is known as ${rest.nativeName}</div>
+                </div>
             </section>
             `
 }
-
-
 export {tripUpdate}
